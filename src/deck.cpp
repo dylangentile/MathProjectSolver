609ab2bd7443b446014d79922744e5a2cf36ffb5
Dylan Gentile <dylangentile@ymail.com>
2019-02-12 20:49:08 +0000

    asdfsadf

--- src/deck.cpp
+++ src/deck.cpp
@@ -15,16 +15,45 @@ Deck::~Deck(){
 void
 Deck::deckInit(){
   Card *cardArray = new Card[52];
-  
+  int x = 0;
   for(int i = 0; i < 4; i++){
     for(int j = 0; j < 13; j++){
-      cardArray[(i*13)*j].suit = i;
-      cardArray[(i*13)*j].value = j+1; 
+      cardArray[x].suit = i;
+      cardArray[x].value = j+1;
+	x++; 
     }
   }
   
   for(int i =0; i < 52; i++){
-    cout << cardArray[i].value << ":" << cardArray[i].suit << "\n";
+    if(cardArray[i].value == 1){
+      cout << "ace";
+    }
+    else if(cardArray[i].value == 11){
+      cout << "jack";
+    }
+    else if(cardArray[i].value == 12){
+      cout << "queen";
+    }
+    else if(cardArray[i].value == 13){
+      cout << "king";
+    }else {
+      cout << cardArray[i].value;
+    }
+    
+    cout << " of ";
+    if(cardArray[i].suit == 0){
+      cout << "spades";
+    }
+    else if(cardArray[i].suit == 1){
+      cout << "hearts";
+    }
+    else if(cardArray[i].suit == 2){
+      cout << "clubs";
+    }
+    else if(cardArray[i].suit == 3){
+      cout << "diamonds";
+    }
+    cout << "\n";
   }
   
 }
